7710fbdec5af7abd7c35cfd5773712e298523ea8
Юрий Дубровских <u.dubrovskikh@planfact.io>
2026-09-02 12:23:37 +0500

    Еще настройки безопасности в gateway

--- front/src/main/java/com/bank/front/configs/SecurityConfig.java
+++ front/src/main/java/com/bank/front/configs/SecurityConfig.java
@@ -20,27 +20,9 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
                         .permitAll()
-                //)
-//                .oauth2ResourceServer(oauth2 -> oauth2
-//                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 );
 
         // Строим и возвращаем цепочку фильтров безопасности
         return http.build();
     }
-
-//    private JwtAuthenticationConverter jwtAuthenticationConverter() {
-//        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-//        // Можно настроить маппинг ролей, если необходимо:
-//        // jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
-//        // jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
-//
-//        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-//        converter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
-//
-//        // Указываем, какую заявку использовать как имя пользователя
-//        converter.setPrincipalAttributeName("preferred_username");
-//
-//        return converter;
-//    }
 }
--- front/src/main/java/com/bank/front/controllers/MainController.java
+++ front/src/main/java/com/bank/front/controllers/MainController.java
@@ -1,7 +1,6 @@
 package com.bank.front.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -53,9 +52,6 @@ public class MainController {
      */
     @GetMapping("/account")
     public String getAccount(Model model) {
-        var auth = SecurityContextHolder.getContext().getAuthentication();
-        auth.getPrincipal();
-
         return "main";
     }
 //
